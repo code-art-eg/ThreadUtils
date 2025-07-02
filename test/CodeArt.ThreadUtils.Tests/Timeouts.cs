10f73607bcb621d7212bd137dd59b482965a2c68
sherif-elmetainy <sherif.elmetainy@gmail.com>
2025-07-03 02:44:13 +0300

    increased timeout for short tests (because it fails in GitHub's Windows servers!)

--- test/CodeArt.ThreadUtils.Tests/Timeouts.cs
+++ test/CodeArt.ThreadUtils.Tests/Timeouts.cs
@@ -2,8 +2,8 @@
 
 public static class Timeouts
 {
-    public const int ShortTestTimeout = 50;
+    public const int ShortTestTimeout = 200;
     public const int LongTestTimeout = 10000;
-    public const int MediumTestTimeout = 1000;
+    public const int MediumTestTimeout = 2000;
     public const int LongTimeout = 2000;
 }
